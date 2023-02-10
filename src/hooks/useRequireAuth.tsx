ebb59c94a492843074f8f334f91218eb1b4a371a
Anthony de Cuyper <decuyperanthony@gmail.com>
2023-02-10 20:21:09 +0100

    Fix(app) : check user for requireAuth

--- src/hooks/useRequireAuth.tsx
+++ src/hooks/useRequireAuth.tsx
@@ -7,8 +7,7 @@ import { LOGIN_PATH } from '../router/constants';
 // eslint-disable-next-line no-undef
 const RequireAuth = ({ children }: { children: JSX.Element }) => {
   const { user } = useAuthContext();
-
-  if (user.isLoaded && !user.isLogged) {
+  if (Object.entries(user).length === 0 || (user.isLoaded && !user.isLogged)) {
     return <Navigate to={LOGIN_PATH} replace />;
   }
 
