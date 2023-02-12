ee06cd8cd6d4d25880cfeb1e226d98756b55c70b
Anthony de Cuyper <decuyperanthony@gmail.com>
2023-02-12 14:53:33 +0100

    fix deploy

--- src/service/config.ts
+++ src/service/config.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance } from 'axios';
 const API_URL =
-  process.env.NODE_ENV === 'development'
+  process?.env?.NODE_ENV === 'development'
     ? 'http://localhost:5050'
     : 'https://pokeapi-production-5f83.up.railway.app';
 
