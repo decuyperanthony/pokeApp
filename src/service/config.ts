09f9df1b4b9244b81c4d1d0c6cce2f43f8ff6323
Anthony de Cuyper <decuyperanthony@gmail.com>
2023-02-12 15:17:41 +0100

    test

--- src/service/config.ts
+++ src/service/config.ts
@@ -3,8 +3,6 @@ const API_URL = import.meta.env.DEV
   ? 'http://localhost:5050'
   : 'https://pokeapi-production-5f83.up.railway.app';
 
-console.log('import.meta.env', import.meta.env);
-
 export const instance = (): AxiosInstance =>
   axios.create({
     baseURL: API_URL,
