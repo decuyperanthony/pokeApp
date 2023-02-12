55ffbd15f70026822ca9c579bf4fb95f325f61ad
Anthony de Cuyper <decuyperanthony@gmail.com>
2023-02-12 15:13:44 +0100

    test

--- src/service/config.ts
+++ src/service/config.ts
@@ -1,8 +1,7 @@
 import axios, { AxiosInstance } from 'axios';
-const API_URL =
-  import.meta.env.MODE === 'development'
-    ? 'http://localhost:5050'
-    : 'https://pokeapi-production-5f83.up.railway.app';
+const API_URL = import.meta.env.DEV
+  ? 'http://localhost:5050'
+  : 'https://pokeapi-production-5f83.up.railway.app';
 // const foo = import.meta.env?.REACT_APP_ENV;
 
 console.log('import.meta.env', import.meta.env);
